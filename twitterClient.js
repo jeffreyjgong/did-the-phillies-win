b1f811c7c1557a3f127bd2a6714c5401fe85f796
jeffreyjgong <jeffrey.gong7@gmail.com>
2022-04-18 22:58:31 -0400

    Added bot access

--- twitterClient.js
+++ twitterClient.js
@@ -6,8 +6,8 @@ require("dotenv").config();
 const client = new TwitterApi({
    appKey: process.env.API_KEY,
    appSecret: process.env.API_SECRET_KEY,
-   accessToken: process.env.ACCESS_TOKEN,
-   accessSecret: process.env.ACCESS_TOKEN_SECRET
+   accessToken: process.env.BOT_ACCESS_TOKEN,
+   accessSecret: process.env.BOT_ACCESS_TOKEN_SECRET
 });
 
 const rwClient = client.readWrite;
