d50c4b17f6a9ebda02aaf1bfbf72f582d3a6d5bd
jeffreyjgong <jeffrey.gong7@gmail.com>
2022-04-18 20:59:23 -0400

    Adding cron

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const rwClient = require("./twitterClient");
+const CronJob = require("cron").CronJob;
+
 
 const tweet = async () => {
    try {
-      await rwClient.v2.tweet("Go Phillies!");
+      await rwClient.v2.tweet("Please win phillies!");
       console.log("Just tweeted!");
    } catch (e) {
       console.error(e);
    }
 };
 
-tweet();
+const job = new CronJob("* * * * *", ()=> {
+   console.log('tweeting now!');
+   tweet();
+});
+
+job.start();
